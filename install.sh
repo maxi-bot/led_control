f0090fe64287bf28bffbbc29e68fff7f6c10e1ed
Leonard Anderweit <leonard.anderweit@stud.tu-darmstadt.de>
2020-02-01 23:23:14 +0100

    install reworked

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 sudo apt update
-sudo apt upgrade
-sudo apt install pigpio apache2 python
-pip install websockets
-cp html/ /var/www/html
-service apache start
+sudo apt upgrade -y
+sudo apt install pigpio apache2 python3
+pip3 install websockets
+rm pigpio.py
+sudo systemctl enable pigpiod
+cp html/* /var/www/html
+sudo service apache start
